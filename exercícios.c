5c252b45b84c81e0fb3d50fd51cdbfd304e140d1
BrunoA31 <brunoarantes31@hotmail.com>
2026-04-02 18:07:02 +0000

    Exercícios

--- exercícios.c
+++ exercícios.c
@@ -1,59 +1,6 @@
 #include <stdio.h>
  
 int main() {
-    
-    //Criando as colunas utilizando um array de caracteres
-    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    
-    //Criando as linhas utilizando um array de strings para deixar o espaçamento correto e ficar mais organizado
-    char *linhas[10] = {" 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10"};
-    
-    //Criando o tabuleiro utilizando uma matriz 10x10 e o valor 0(zero) para representar a água
-    int tabuleiro[10][10];
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            tabuleiro[i][j] = 0;
-        }
-        
-    }
-    //Criando os navios utilizando um vetor (array unidimensional) de tamanho 3 e o valor 3 para representá-los
-    int navio1[3] = {3, 3, 3};
-    int navio2[3] = {3, 3, 3};
-
-    //Posicionando os navios no tabuleiro
-    tabuleiro[2][2] = navio1[0];
-    tabuleiro[2][3] = navio1[1];
-    tabuleiro[2][4] = navio1[2];
-
-    tabuleiro[5][7] = navio2[0];
-    tabuleiro[6][7] = navio2[1];
-    tabuleiro[7][7] = navio2[2];
-
-    //Imprimindo o cabeçalho e um espaço para alinhamento das colunas
-    printf("  ***TABULEIRO BATALHA NAVAL*** \n");
-    printf("  ");
-    
-    //Imprimindo as letras que representam as colunas
-    for (int i = 0; i < 10; i++)
-    {
-        printf("  %c", colunas[i]);
-    }
-    printf("\n");
-    
-    //Imprimindo as linhas e o tabuleiro com um loop aninhado
-    for (int j = 0; j < 10; j++)
-    {
-        printf("%s ", linhas[j]);
-        for (int k = 0; k < 10; k++)
-        {
-            printf(" %d ", tabuleiro[j][k]);
-        }
-        printf("\n");
-    }
-    
-    
 
     return 0;
 }
